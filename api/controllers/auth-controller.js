5280a52cfba470d85d5b3154eeb7c6116cad4a41
yrd369 <yogharajdhayalnamburajan@gmail.com>
2024-04-22 14:30:22 +0530

    feat: completed signin page functionality

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user-model.js";
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
 // signup
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password ? password : " ", 10);
+  const hashedPassword = bcryptjs.hashSync(password ? password : " ", 10);
 
   try {
     !username ||
@@ -27,7 +28,27 @@ export const signUp = async (req, res, next) => {
 };
 
 // signin
-export const signIn = (req, res) => {
+export const signIn = async (req, res, next) => {
   const { email, password } = req.body;
-  res.json(req.body);
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,4 +1,82 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignIn = () => {
-  return <>signin</>;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setError("Please fill all the fields ");
+    }
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await fetch("http://localhost:3000/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setError(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <div className="font-poppins p-3 max-w-lg mx-auto mt-5 rounded-md">
+        <h1 className="text-3xl my-5 font-semibold text-center">Sign In</h1>
+        <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+          <input
+            className="bg-[#eeee] rounded-lg px-3 py-2 outline-none"
+            placeholder="example@gmail.com"
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleChange}
+          />
+          <input
+            className="bg-[#eeee] rounded-lg px-3 py-2 outline-none"
+            placeholder="password"
+            id="password"
+            type="password"
+            name="password"
+            onChange={handleChange}
+          />
+          <button
+            className="bg-blue-500 px-3 py-2 rounded-lg hover:opacity-95 disabled:opacity-80"
+            disabled={loading}
+          >
+            {loading ? "loading" : "Sign In"}
+          </button>
+        </form>
+        <div className="mt-5">
+          <p>
+            Dont Have an account?{" "}
+            <span className="text-blue-700">
+              <Link to={"/sign-in"}>Sign up</Link>
+            </span>
+          </p>
+        </div>
+        {error && (
+          <p className="bg-red-500 text-white p-3 rounded-md">{error}</p>
+        )}
+      </div>
+    </>
+  );
 };
 export default SignIn;
